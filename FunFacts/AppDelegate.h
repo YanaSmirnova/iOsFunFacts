71252b00f3c04ff8590cf8e98a1633aa624ed0f2
YanaSmirnova <smirnova.yana@gmail.com>
2015-03-12 18:11:15 +1100

    Initial commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by YANA SMIRNOVA on 12/03/2015.
+//  Copyright (c) 2015 YANA SMIRNOVA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by YANA SMIRNOVA on 12/03/2015.
+//  Copyright (c) 2015 YANA SMIRNOVA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
